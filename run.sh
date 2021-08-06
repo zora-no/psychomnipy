bcdfa8da90d4e92456b234d832453c0382480661
Hunaid Hameed <hunaid.hameed@dfki.de>
2021-08-06 17:31:09 +0200

    run.sh bug fix

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 Xvfb :1 -screen 0 1024x768x24 -ac +extension GLX +render -noreset &> xvfb.log &
 
 # Export your display id
-RUN export DISPLAY=:1
+export DISPLAY=:1
 ######
 
 python3 psychopy_docker_test.py
