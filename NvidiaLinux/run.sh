9713e46957fe8ab245df56191c4a7ea9784442ee
H <hunaidhameed@hotmail.com>
2021-11-23 19:16:49 +0100

    nvidia run

--- NvidiaLinux/run.sh
+++ NvidiaLinux/run.sh
@@ -1,7 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-COMMAND=/bin/bash
-#COMMAND=""
-
-xhost + # allow connections to X server
-docker run --privileged -e "DISPLAY=unix:0.0" -v="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v="/dev/:/dev/" -i -t ubuntu_with_opensource_drivers $COMMAND
+#docker run --privileged -e "DISPLAY=unix:0.0" -v="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v="/dev/:/dev/" -i -t ubuntu_with_nvidia_driver /bin/bash
+xhost +
+docker run --privileged -e DISPLAY=$DISPLAY -v="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v="/dev/:/dev/" -i -t ubuntu_with_nvidia_driver bash
