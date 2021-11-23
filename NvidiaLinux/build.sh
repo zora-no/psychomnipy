0455a5a20718b2ad591bf9060c9a2fb757d2754f
H <hunaidhameed@hotmail.com>
2021-11-23 19:16:20 +0100

    nvidia build

--- NvidiaLinux/build.sh
+++ NvidiaLinux/build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-sudo docker build -t ubuntu_with_opensource_drivers .
+IMAGE_NAME=ubuntu_with_nvidia_driver
+NVIDIA_DRIVER=NVIDIA-Linux-x86_64-460.27.04.run  # path to nvidia driver
+
+cp ${NVIDIA_DRIVER} NVIDIA-DRIVER.run
+docker build -t ${IMAGE_NAME} .
